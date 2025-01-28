05d915383d2f0114f61a516252a30741bf2a9630
Adam Konopka <adam.konopka@hitta.se>
2025-01-28 07:26:20 +0100

    Vercel adjustments

--- api/words.js
+++ api/words.js
@@ -2,7 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = async (req, res) => {
-  const wordsPath = path.join(__dirname, '../words.json');
-  const words = JSON.parse(fs.readFileSync(wordsPath, 'utf8'));
-  res.status(200).json(words);
+  const wordsPath = path.join(__dirname, '../words.json'); // Poprawna ścieżka do words.json
+  try {
+    const words = JSON.parse(fs.readFileSync(wordsPath, 'utf8'));
+    res.status(200).json(words);
+  } catch (error) {
+    res.status(500).json({ error: 'Nie udało się załadować słówek' });
+  }
 };
